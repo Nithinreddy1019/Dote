37fd045d2a9579d5a8d2a37315a3b6ec443cfeee
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-06-21 19:26:59 +0530

    ui: added user Details and logout

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,29 @@
+"use client"
 
+import { Button } from "@/components/ui/button";
+
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
 
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return (
-        <div>
-            protected page
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image 
+                src={"/createnote.svg"}
+                height={350}
+                width={350}
+                alt="create note"
+            />
+            <h2 className="text-lg font-medium">
+                Welcome to {user?.firstName}&apos;s Dote
+            </h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2"/>
+                Create a page
+            </Button>
         </div>
     );
 }
