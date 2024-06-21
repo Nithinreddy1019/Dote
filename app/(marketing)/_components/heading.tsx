10e94f42f4154fd3783a97f6128fe2ec47cd85d5
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-06-21 12:02:10 +0530

    Authentication

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -3,7 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
+
 export const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth(); 
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +19,28 @@ export const Heading = () => {
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                 Workspace where better, productive <br /> & faster work happens.
             </h3>
-            <Button>
-                Go to Dote
-                <ArrowRight className="h-4 w-4 ml-2"/>
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner/>
+                </div>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Dote
+                        <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Button>
+                </SignInButton>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Go to Dote
+                        <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Link>
+                </Button>
+            )}
+            
         </div>
     )
 };
