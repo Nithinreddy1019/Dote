645caed25758f2381b6c110f77dc78e1bc2ae9ff
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-06-23 22:06:14 +0530

    ui: create from FE

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -5,9 +5,26 @@ import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
     const { user } = useUser();
+
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+        const promise = create({
+            title: "untitled"
+        });
+
+        toast.promise(promise, {
+            loading: "Creating new page",
+            success: "New page created",
+            error: "Failed to create new page"
+        })
+    }
 
     return (
         <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -20,7 +37,7 @@ const DocumentsPage = () => {
             <h2 className="text-lg font-medium">
                 Welcome to {user?.firstName}&apos;s Dote
             </h2>
-            <Button>
+            <Button onClick={onCreate}>
                 <PlusCircle className="h-4 w-4 mr-2"/>
                 Create a page
             </Button>
