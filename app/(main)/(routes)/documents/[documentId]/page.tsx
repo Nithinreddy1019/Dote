3a96f33236fb05a3b5a98f2abaa93fdc578a3ec1
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-07-08 21:18:12 +0530

    ui: Editor comp

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Cover } from "@/components/cover";
+import { Editor } from "@/components/editor";
 import { Toolbar } from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
@@ -54,6 +55,10 @@ const DocumentIdPage = ({
             <Cover url={document.coverImage}/>
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document}/>
+                <Editor 
+                    onChange={() => {}}
+                    initialContent={document.content}
+                />
             </div>
         </div>
     );
