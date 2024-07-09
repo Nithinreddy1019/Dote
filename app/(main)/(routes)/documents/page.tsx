8af3e2d7283569dab0c4683ce1d1e6449c69e472
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-07-09 12:00:12 +0530

    ui: added redirection upon creation

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -8,16 +8,18 @@ import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const DocumentsPage = () => {
     const { user } = useUser();
 
+    const router = useRouter();
     const create = useMutation(api.documents.create);
 
     const onCreate = () => {
         const promise = create({
             title: "Untitled"
-        });
+        }).then((documentId) => router.push(`documents/${documentId}`))
 
         toast.promise(promise, {
             loading: "Creating new page",
--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -56,7 +56,7 @@ export const Item = ({
         
         if(!id) return;
 
-        const promise = archive({id});
+        const promise = archive({id}).then(() => router.push("/documents"))
 
         toast.promise(promise, {
             loading: "Moving to trash...",
@@ -79,7 +79,7 @@ export const Item = ({
             if(!expanded) {
                 onExpand?.()
             }
-            //router.push(`/documents/${documentId}`)
+            router.push(`/documents/${documentId}`)
         });
 
         toast.promise(promise, {
