27faea700d23d6bb326c4a17a6bbe587d27c8fc9
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-07-09 11:59:51 +0530

    ui: Editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -9,7 +9,8 @@ import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
 import { useTheme } from "next-themes";
 import "@blocknote/mantine/style.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
 
 
 interface EditorProps {
@@ -18,39 +19,52 @@ interface EditorProps {
     editable?: boolean
 }
 
-export const Editor = ({
+const Editor = ({
     onChange,
     initialContent,
     editable
 }: EditorProps) => {
 
     const { resolvedTheme } = useTheme();
+    const { edgestore } = useEdgeStore();
+
     const [blocks, setBlocks] = useState<Block[]>([]);
 
-    if(JSON.parse(initialContent as string).length === 0) {
+    const handleUpload = async (file: File) => {
+        const response = await edgestore.publicFiles.upload({
+            file
+        });
+
+        return response.url;
+    };
+
+
+    if(initialContent && JSON.parse(initialContent as string).length === 0) {
         initialContent = undefined;
     }
+
     const editor: BlockNoteEditor = useCreateBlockNote({
         initialContent: initialContent ? JSON.parse(initialContent) as PartialBlock[] : undefined,
-    })
+        uploadFile: handleUpload
+    });
 
-
-    useEffect(() => {
-        onChange(JSON.stringify(blocks, null, 2));
-    }, [blocks])
-    
     const hanldeDocumentChange = () => {
         const newBlocks = editor.document;
         setBlocks(newBlocks);
-    };
+        onChange(JSON.stringify(newBlocks));
+    }
 
     return (
         <div>
             <BlockNoteView 
                 editor={editor}
+                editable={editable}
                 theme={resolvedTheme === "dark" ? "dark" : "light"}
                 onChange={hanldeDocumentChange}
             />
         </div>
     )
-}
+};
+
+
+export default Editor;
